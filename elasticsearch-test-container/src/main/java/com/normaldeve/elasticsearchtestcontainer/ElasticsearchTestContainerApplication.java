bca1de3f183875e271c30b5c4a90d9434ace3249
junnukim1007gmail.com <junnukim1007@gmail.com>
2025-12-31 10:13:41 +0900

    Elasticsearch Test Container 예제 생성

---
+++ elasticsearch-test-container/src/main/java/com/normaldeve/elasticsearchtestcontainer/ElasticsearchTestContainerApplication.java
@@ -0,0 +1,13 @@
+package com.normaldeve.elasticsearchtestcontainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchTestContainerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchTestContainerApplication.class, args);
+    }
+
+}
---
+++ elasticsearch-test-container/src/test/java/com/normaldeve/elasticsearchtestcontainer/ElasticsearchTestContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.normaldeve.elasticsearchtestcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchTestContainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
